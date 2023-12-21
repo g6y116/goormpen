068fd91b992d01de58c812200b8b8bc09aecae0e
g6y116 <g6y116@naver.com>
2023-12-22 08:48:27 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/sj/goormpen/GoormpenApplication.java
@@ -0,0 +1,13 @@
+package sj.goormpen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormpenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sj/goormpen/GoormpenApplicationTests.java
@@ -0,0 +1,13 @@
+package sj.goormpen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormpenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
